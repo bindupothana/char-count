b46e0f9b5c7fa0354f78dd63c0ac5d2211eb982f
bindupothana <himabindu.pothana@navaratan.com>
2017-03-24 16:42:43 +0530

    string coin ting

--- script/app.js
+++ script/app.js
@@ -1,14 +1,26 @@
 var app=angular.module('charApp',[]);
 app.controller('charCtrl',function($scope){
-	var count=0;
-	$scope.charCount=function(name){
-		$scope.sringLength=$scope.input.length
-      for (var i=0; i<$scope.input.length(); i++){
-      	console.log("hjihlk",$scope.input)
-      	if (isValidWordCharacter($scope.input.charAt(i))){
-                incrementCounter = true;
-                count++;
-            }
-            }
+	    $scope.charCount=function(string){
+      $scope.obj={};
+       var repeats=[];
+       var length = string.length;
+      for(var i = 0; i < length; i++) {
+       console.log("gggg",string.length)
+       var strl = string.charAt(i)
+      $scope.obj[strl] = (isNaN($scope.obj[strl]) ? 1 : $scope.obj[strl] + 1);
+  }
+
+
+ 
+
+
+console.log($scope.obj)
         }
+
+  
 });
+
+
+
+
+
